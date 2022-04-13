bb7b779c1a5fca569aea7d7e6597a67bb7dc56d9
dmitrykoryanov <dmitry.koryanov@yandex.ru>
2022-04-13 17:56:38 +0300

    TEAMUP-27 Проработать до конца систему уведомлений * Не был закоммичен входной класс сервиса с методом main - TeamupNotifyApplication.java

---
+++ src/main/java/ru/team/up/notify/TeamupNotifyApplication.java
@@ -0,0 +1,16 @@
+package ru.team.up.notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TeamupNotifyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TeamupNotifyApplication.class, args);
+    }
+
+}
